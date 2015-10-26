f531348ed3425a551de7602d928e1f1ab2d4926a
wyt <wyt.daily@gmail.com>
2015-10-26 18:37:10 +0800

    1

---
+++ scripts/stop_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+isExistApp=`pgrep apache2`
+if [[ -n  \$isExistApp ]]; then
+   service apache2 stop
+fi
+isExistApp=`pgrep mysqld`
+if [[ -n  \$isExistApp ]]; then
+    service mysqld stop
+fi
